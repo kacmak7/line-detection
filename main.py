9c733f5fd3dd2124b1388eaf7b47e77ccc815571
Kacper Makuch <kacbull007@gmail.com>
2018-12-29 23:42:26 +0100

    Build_1

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import os
 import cv2
-import numpy
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-def gaussian_blur(img, kernel_size):
-    """Applies a Gaussian Noise kernel"""
-    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+def region_of_interest(img, vertices):
+   
+    #defining a blank mask to start with
+    mask = np.zeros_like(img)
+
+    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    #filling pixels inside the polygon defined by "vertices" with the fill color
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+    #plt.figure()
+    #plt.imshow(mask)
+    #returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
 
 
-image_path = 'input/Untitled-design.jpg';
 
-img = cv2.imread(image_path, 0)
+
+
+image_path = 'input/yello-white-road-line_resize_md.jpg';
+
+img = cv2.imread(image_path)
 #print type(img.shape)
 
 if len(img.shape) > 2:
@@ -17,22 +38,38 @@ if len(img.shape) > 2:
 else:
     img_grey = img
 
-img_gaus = gaussian_blur(img, 5)
-
+kernel_size = 5
+img_gaus = cv2.GaussianBlur(img_grey, (kernel_size, kernel_size), 0)
 img_edge = cv2.Canny(img_gaus, 50, 150)
+
+mask = np.zeros_like(img_edge)
+
+#defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+if len(img.shape) > 2:
+    channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+    ignore_mask_color = (255,) * channel_count
+else:
+    ignore_mask_color = 255
+
+imshape = img.shape
+vertices = np.array([[(0,imshape[0]),(450,290),(490,290),(imshape[1],imshape[0])]],dtype=np.int32)
+cv2.fillPoly(mask, vertices, ignore_mask_color)
+masked_edges = cv2.bitwise_and(img_edge, mask)
+
+
 
 #show
 cv2.imshow('virgin image', img)
 cv2.imshow('gaussian blur', img_gaus)
 cv2.imshow('edges', img_edge)
+cv2.imshow('mask', mask)
+plt.imshow(masked_edges)
+plt.show()
 
+#exit
 k = cv2.waitKey(0)
 if k == 27:         # wait for ESC key to exit
     cv2.destroyAllWindows()
 elif k == ord('s'): # wait for 's' key to save and exit
-    cv2.imwrite('output/image.jpg',img)
+    cv2.imwrite('output/image.jpg', img_edge)
     cv2.destroyAllWindows()
-
-def gaussian_blur(img, kernel_size):
-    """Applies a Gaussian Noise kernel"""
-    return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
